7f71c3bac8771f0080bf229286b7d85ef10c2cca
Josh Long <54473+joshlong@users.noreply.github.com>
2025-04-18 01:20:46 -0700

    adding beans
    Signed-off-by: Josh Long <54473+joshlong@users.noreply.github.com>

---
+++ beans/README.md
@@ -0,0 +1,12 @@
+# Beans 
+
+## objectives
+* `raii` - RAII
+* `di` - dependency injection
+* `portableserviceabstractions` - portable service abstraction
+* `aop` - aspect oriented programming
+* `springframework` - Spring Framework
+* `componentscan` - Spring Framework component scanning 
+* `lifecycle` - Spring Framework's `InitializingBean`, `DisposableBean`, `@PreDestroy`, `@PostConstruct`, etc. 
+* `beanpostprocessors` - Spring Framework `BeanPostProcessor`
+* `springboot` - Spring Framework `BeanPostProcessor`
---
+++ beans/src/main/java/com/example/beans/beanpostprocessors/CustomerServiceConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.beans.beanpostprocessors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan
+class CustomerServiceConfiguration {
+
+    @Bean
+    static LoggingBeanPostProcessor loggingBeanPostProcessor() {
+        return new LoggingBeanPostProcessor();
+    }
+
+    @Bean
+    EmbeddedDatabase dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+
+    @Bean
+    JdbcClient jdbcClient(DataSource dataSource) {
+        return JdbcClient.create(dataSource);
+    }
+
+}
