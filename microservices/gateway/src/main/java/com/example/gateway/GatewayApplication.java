31475dd56f463948aa5d5a24ffe5bd9ba1542f34
Josh Long <54473+joshlong@users.noreply.github.com>
2025-04-19 14:23:10 -0700

    microservices

---
+++ microservices/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,32 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> apiGW() {
+        return route()
+                .before(BeforeFilterFunctions.rewritePath("/api", "/"))
+                .GET("/api/**", http())
+                    .filter(lb("service"))
+                .GET("/**", http("http://127.0.0.1:8020"))
+                .build();
+    }
+}
