478ceb8ff15d6d9e6c2226be9069f0a6e6604f4f
Josh Long <54473+joshlong@users.noreply.github.com>
2025-04-20 00:48:41 -0700

    formatting

--- messaging-integration/messaging/src/main/java/com/example/eai/EaiApplication.java
+++ messaging-integration/messaging/src/main/java/com/example/eai/EaiApplication.java
@@ -19,98 +19,90 @@ import org.springframework.pulsar.core.PulsarTemplate;
 @SpringBootApplication
 public class EaiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EaiApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EaiApplication.class, args);
+	}
 
 }
 
 @Configuration
 class RabbitConfiguration {
 
-    static final String DESTINATION = "rabbit-adoptions";
+	static final String DESTINATION = "rabbit-adoptions";
 
-    @Bean
-    Binding adoptionsBinding(Queue adoptionsQueue, Exchange adoptionsExchange) {
-        return BindingBuilder
-                .bind(adoptionsQueue)
-                .to(adoptionsExchange)
-                .with(DESTINATION)
-                .noargs();
-    }
+	@Bean
+	Binding adoptionsBinding(Queue adoptionsQueue, Exchange adoptionsExchange) {
+		return BindingBuilder.bind(adoptionsQueue).to(adoptionsExchange).with(DESTINATION).noargs();
+	}
 
-    @Bean
-    Queue adoptionsQueue() {
-        return QueueBuilder
-                .durable(DESTINATION)
-                .build();
-    }
+	@Bean
+	Queue adoptionsQueue() {
+		return QueueBuilder.durable(DESTINATION).build();
+	}
 
-    @Bean
-    Exchange adoptionsExchange() {
-        return ExchangeBuilder
-                .directExchange(DESTINATION)
-                .build();
-    }
+	@Bean
+	Exchange adoptionsExchange() {
+		return ExchangeBuilder.directExchange(DESTINATION).build();
+	}
 
-    @RabbitListener(queues = DESTINATION)
-    void on(Message<AdoptionRequest> ar) {
-        Listener.register(DESTINATION, ar);
-    }
+	@RabbitListener(queues = DESTINATION)
+	void on(Message<AdoptionRequest> ar) {
+		Listener.register(DESTINATION, ar);
+	}
 
-    @Bean
-    Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper) {
-        return new Jackson2JsonMessageConverter(objectMapper,
-                EaiApplication.class.getPackage().getName());
-    }
+	@Bean
+	Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper) {
+		return new Jackson2JsonMessageConverter(objectMapper, EaiApplication.class.getPackage().getName());
+	}
 
-    @Bean
-    ApplicationRunner rabbitRunner(RabbitTemplate rt) {
-        return _ -> rt
-                .convertAndSend(DESTINATION, new AdoptionRequest("Prancer"));
-    }
+	@Bean
+	ApplicationRunner rabbitRunner(RabbitTemplate rt) {
+		return _ -> rt.convertAndSend(DESTINATION, new AdoptionRequest("Prancer"));
+	}
+
 }
 
 @Configuration
 class PulsarConfiguration {
 
-    static final String TOPIC = "pulsar-adoptions";
+	static final String TOPIC = "pulsar-adoptions";
 
-    @Bean
-    ApplicationRunner pulsarRunner(PulsarTemplate<AdoptionRequest> pulsarTemplate) {
-        return args -> pulsarTemplate.send(TOPIC, new AdoptionRequest("Prancer"));
-    }
+	@Bean
+	ApplicationRunner pulsarRunner(PulsarTemplate<AdoptionRequest> pulsarTemplate) {
+		return args -> pulsarTemplate.send(TOPIC, new AdoptionRequest("Prancer"));
+	}
 
-    @PulsarListener(topics = TOPIC, subscriptionName = "adoptions-subscription")
-    void on(Message<AdoptionRequest> ar) {
-        Listener.register(TOPIC, ar);
-    }
+	@PulsarListener(topics = TOPIC, subscriptionName = "adoptions-subscription")
+	void on(Message<AdoptionRequest> ar) {
+		Listener.register(TOPIC, ar);
+	}
+
 }
 
 @Configuration
 class KafkaConfiguration {
 
-    static final String DESTINATION = "kafka-adoptions";
+	static final String DESTINATION = "kafka-adoptions";
 
-    @Bean
-    ApplicationRunner kafkaRunner(KafkaTemplate<String, AdoptionRequest> kafkaTemplate) {
-        return _ -> kafkaTemplate.send(DESTINATION, new AdoptionRequest("Prancer"));
-    }
+	@Bean
+	ApplicationRunner kafkaRunner(KafkaTemplate<String, AdoptionRequest> kafkaTemplate) {
+		return _ -> kafkaTemplate.send(DESTINATION, new AdoptionRequest("Prancer"));
+	}
 
-    @KafkaListener(topics = DESTINATION, groupId = "adoptions-group")
-    void on(Message<AdoptionRequest> ar) {
-        Listener.register(DESTINATION, ar);
-    }
+	@KafkaListener(topics = DESTINATION, groupId = "adoptions-group")
+	void on(Message<AdoptionRequest> ar) {
+		Listener.register(DESTINATION, ar);
+	}
 
 }
 
 abstract class Listener {
 
-    static void register(String name, Message<?> message) {
-        System.out.println("------");
-        System.out.println("Received: " + name + ": [" + message.getPayload() + ']');
-        message.getHeaders().forEach((k, v) -> System.out.println(k + ": " + v));
-    }
+	static void register(String name, Message<?> message) {
+		System.out.println("------");
+		System.out.println("Received: " + name + ": [" + message.getPayload() + ']');
+		message.getHeaders().forEach((k, v) -> System.out.println(k + ": " + v));
+	}
 
 }
 
